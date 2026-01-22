ef1f69ac3c5c57f56d083af83766508972f7ace3
Marie-Lorraine Collignon <marie-lorraine.collignon@fms-ea.com>
2026-01-22 13:29:03 +0100

    Improved home style

--- ui/renderHome.js
+++ ui/renderHome.js
@@ -9,15 +9,15 @@ export const renderHome = (app, { containerSelector = "#client-home" } = {}) => 
   if (!container) return;
 
   container.innerHTML = `
-    <div class="Panel">
-      <div class="text-muted small mb-2">Accueil</div>
+
+
 
       ${app.uiState.isAuthenticated
-      ? `<h3 class="mb-2">Bienvenue</h3>
-             <p class="text-muted mb-0">Lorraine ipsum : tu es “connecté” .</p>`
-      : `<h3 class="mb-2">Bienvenue</h3>
-             <p class="text-muted mb-0">Clique sur “Se connecter” pour passer en mode authentifié.</p>`
+      ? `<h3 class="mb-2">Home & auth</h3>
+             `
+      : `<h3 class="mb-2">Home & !auth</h3>
+           `
     }
-    </div>
+
   `;
 };
